f94979c78d2a629e12cfbfbfdbed73c17e17e2b7
monicaravichandran <monicaravichandran3@gmail.com>
2017-04-03 09:50:59 -0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -3,6 +3,27 @@ persnickety
 #* Section 2 (Data Definitions)
 
 #* 1)
+class Celsius:
+   def __init__(self,celsius):
+      self.celsius = celsius
+   def __eq__(self, other):
+      return self.celsius == other.celsius
+   def __repr__(self):
+      return("Celsius(%r)" % (self.celsius))
+class Farenheit:
+   def __init__(self,farenheit):
+      self.farenheit = farenheit
+   def __eq__(self,other):
+      return self.farenheit == other.farenheit
+   def __repr__(self): 
+      return("Farenheit(%r)" % (self.farenheit))
+def main():
+   celsius = float(input("Celsius:"))
+   c = Celsius(celsius)
+   f = Farenheit(c.celsius*1.8+32)
+   print(f)
+if  __name__ == '__main__':
+   main()
 
 #* 2)
 
